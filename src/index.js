b0cfaa957e4428a5a9d82171f8045eafa12161e9
Dean Asadi <eng.de86@gmail.com>
2018-09-12 10:21:32 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./ducks/store";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>,
+  <App />,
+
   document.getElementById("root")
 );
 registerServiceWorker();
